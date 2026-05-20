7bafa9558db4a1408308f6362ec8f7d3e26b44bc
lawless-lad237 <charlesspencer237@gmail.com>
2026-05-20 16:50:28 +1000

    Update Footer.vue

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -3,22 +3,23 @@
         <footer class="flex items-center justify-evenly py-12 relative z-20">
             <NuxtLink :to="`mailto:${email}`"
                 class="text-sm text-zinc-700 hover:text-zinc-950 dark:text-zinc-50/80 hover:dark:text-zinc-50">
-                {{ hello@2600studios.com }}
+                {{ email }}
             </NuxtLink>
+
             <NuxtLink to="https://github.com/cesswhite/hato-minimal-template" target="_blank"
                 class="text-sm text-zinc-700 hover:text-zinc-950 dark:text-zinc-50/80 hover:dark:text-zinc-50">
                 2600 Studios by Charles Dupilpil
             </NuxtLink>
+
             <NuxtLink :to="`tel:${phone}`"
                 class="text-sm text-zinc-700 hover:text-zinc-950 dark:text-zinc-50/80 hover:dark:text-zinc-50">
-                {{ +61 469 053 763 }}
+                {{ phone }}
             </NuxtLink>
         </footer>
     </UContainer>
 </template>
 
-
 <script setup>
 const app = useAppConfig();
-const { data: { email, phone } } = app
+const { data: { email, phone } } = app;
 </script>

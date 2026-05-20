8f76f398e79e98e2da1516432466b52fc2a93809
lawless-lad237 <charlesspencer237@gmail.com>
2026-05-20 18:03:36 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@vueuse/motion/nuxt', '@nuxt/image'],
-  css: ['~/assets/css/global.css', '~/assets/css/main.css'],
-  image: {
-    domains: ['d3ynb031qx3d1.cloudfront.net']
-  }
-})
+  modules: ["@nuxt/ui", "@vueuse/motion/nuxt", "@nuxt/image"],
+  css: ["~/assets/css/global.css", "~/assets/css/main.css"],
+});

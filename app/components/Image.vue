a1028a6219b699f71af75503359ba4d40cb94d70
lawless-lad237 <charlesspencer237@gmail.com>
2026-05-20 17:18:17 +1000

    Update Image.vue

--- app/components/Image.vue
+++ app/components/Image.vue
@@ -1,16 +1,38 @@
 <template>
     <UModal v-model:open="showPreviewModal" fullscreen>
-        <NuxtImg :src="props.src" :placeholder="img(props.src, { width: 20, blur: 16, quality: 90 })" fit="cover"
-            class="size-60 object-center object-cover cursor-pointer transform-gpu transition-all ease-in-out duration-150 will-change-transform group-hover:scale-102 group-hover:group-odd:rotate-2 group-hover:group-even:-rotate-2"
-            alt="image-work" loading="lazy" />
+        <!-- THUMBNAIL: Added @click to trigger the modal -->
+        <NuxtImg 
+            @click="showPreviewModal = true"
+            :src="props.src" 
+            :placeholder="img(props.src, { width: 20, blur: 16, quality: 90 })" 
+            fit="cover"
+            class="w-full h-full object-center object-cover cursor-pointer transform-gpu transition-all ease-in-out duration-300 will-change-transform group-hover:scale-105"
+            alt="2600 Studios - Gallery Work" 
+            loading="lazy" 
+        />
+        
         <template #content>
-            <div class="h-dvh relative p-4">
-                <div class="absolute top-4 right-4 z-10">
-                    <UButton aria-label="button to close full preview" variant="ghost" color="neutral" icon="i-lucide-x"
-                        class="ring-1 ring-blue-200 text-white" @click="showPreviewModal = false" />
+            <!-- MODAL WRAPPER: Pure black background for high contrast -->
+            <div class="h-dvh relative p-4 bg-black dark:bg-black">
+                <div class="absolute top-6 right-6 z-50">
+                    <!-- CLOSE BUTTON: Removed blue ring, added #E4002B red hover -->
+                    <UButton 
+                        aria-label="Close full preview" 
+                        variant="ghost" 
+                        icon="i-lucide-x"
+                        class="text-white hover:text-[#E4002B] hover:bg-transparent transition-colors duration-200 size-10" 
+                        @click="showPreviewModal = false" 
+                    />
                 </div>
-                <NuxtImg :src="props.src" :placeholder="img(props.src, { width: 20, blur: 16, quality: 90 })"
-                    fit="cover" class="size-full object-center object-cover" loading="lazy" />
+                
+                <!-- FULL PREVIEW: Changed to object-contain so photos aren't cropped -->
+                <NuxtImg 
+                    :src="props.src" 
+                    :placeholder="img(props.src, { width: 20, blur: 16, quality: 90 })"
+                    fit="contain" 
+                    class="size-full object-center object-contain" 
+                    loading="lazy" 
+                />
             </div>
         </template>
     </UModal>
